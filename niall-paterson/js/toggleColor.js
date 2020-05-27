07a36a22bfd734b8e25cb4b1b1d2120a083540c6
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-05-27 22:02:21 +0100

    refactor: Remove whitespace

--- niall-paterson/js/toggleColor.js
+++ niall-paterson/js/toggleColor.js
@@ -2,10 +2,10 @@ export const toggleColor = function () {
     document.querySelectorAll("*").forEach(element => {
         if (element.style.backgroundColor === "") {
             element.style.backgroundColor = "black";
-            element.style.color= "white";
+            element.style.color = "white";
         } else if (element.style.backgroundColor === "black") {
             element.style.backgroundColor = "";
-            element.style.color= "black";
+            element.style.color = "black";
         }
-    });   
+    });
 };
