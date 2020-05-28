2a921a6b3dcc7fa92814464c22138348134da569
David Wildman <59884105+TheWildebeest@users.noreply.github.com>
2020-05-28 19:38:12 +0100

    cleaning up

--- wildmanfolder/eighties.js
+++
@@ -1,8 +0,0 @@
-const needMoreEighties = () => {
-	let itsTheEighties = document.getElementsByClassName('eighties');
-	if (itsTheEighties.classList.contains('offSwitch')) {
-		itsTheEighties.classList.remove('offSwitch');
-	} else {
-		itsTheEighties.classList.add('offSwitch');
-	}
-	}
