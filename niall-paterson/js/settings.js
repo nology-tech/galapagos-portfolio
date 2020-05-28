2c818e25e352c8eace5361e4618efbdb9e4dc52d
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-05-28 16:53:54 +0100

    chore: Finish lint

--- niall-paterson/js/settings.js
+++ niall-paterson/js/settings.js
@@ -1,11 +1,11 @@
 export const settings = {
-    colorMode: "light"
+  colorMode: 'light'
 };
 
-export const updateSettings = function() {
-    if (settings.colorMode === "light") {
-        settings.colorMode = "dark";
-    } else {
-        settings.colorMode = "light";
-    }
+export const updateSettings = function () {
+  if (settings.colorMode === 'light') {
+    settings.colorMode = 'dark';
+  } else {
+    settings.colorMode = 'light';
+  }
 };
