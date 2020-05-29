87cad2bf2df81d7dd57570d333745b380a71a20e
Your Name <you@example.com>
2020-05-29 20:54:14 +0100

    friday night changes

--- louis/scripts.js
+++ louis/scripts.js
@@ -1,1 +1,33 @@
-// currently empty, but will be full of awesome stuff at some point
+
+function dropdown() {
+  document.getElementById('dropdownLinks').classList.toggle('show');
+}
+console.log(dropdown);
+
+
+
+function spin() {
+  document.getElementById('link1').classList.toggle('spinicon');
+}
+console.log(spin);
+
+
+
+function spin2() {
+  document.getElementById('link2').classList.toggle('spinicon2');
+}
+console.log(spin2);
+
+
+
+function spin3() {
+  document.getElementById('link3').classList.toggle('spinicon3');
+}
+console.log(spin3);
+
+
+
+function spin4() {
+  document.getElementById('link4').classList.toggle('spinicon4');
+}
+console.log(spin4);
