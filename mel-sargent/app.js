f3d36c5670017ab01958e9d6115f4b94054ac449
mel <melissa.sargent@hotmail.co.uk>
2020-05-29 06:58:27 +0100

    linting

--- mel-sargent/app.js
+++ mel-sargent/app.js
@@ -1,5 +1,36 @@
 const expand = (x) => {
-  x.classList.toggle("change");
-  const mainNav = document.getElementById("dropdown");
-  mainNav.classList.toggle("show");
-}
+  x.classList.toggle('change');
+  const mainNav = document.getElementById('dropdown');
+  mainNav.classList.toggle('show');
+};
+
+// function expand(x) {
+  // x.classList.toggle('change');
+  // const mainNav = document.getElementById('dropdown');
+  // mainNav.classList.toggle('show');
+// }
+
+// const menu = document.getElementById('burger');
+// menu.onclick = function expand(x) {
+// x.classList.toggle('change');
+// const mainNav = document.getElementById('dropdown');
+// mainNav.classList.toggle('show');
+// };
+
+// const expand = (x) => {
+//   x.classList.toggle('change');
+//   const mainNav = document.getElementById('dropdown');
+//   mainNav.classList.toggle('show');
+// };
+
+// const menu = document.getElementsByClassName('burger');
+// menu.onclick = expand = (x) => {
+// x.classList.toggle('change');
+// };
+
+// const mainNav = document.getElementById('dropdown');
+// mainNav.classList.toggle('show');
+
+// document.getElementsByClassName('burger').addEventListener('click', const expand = (x) => {
+// x.classList.toggle('change');
+// });
