fc270e7f2ce67152e7c60cb1cc7e6bff11e5bd1a
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-05-29 08:10:53 +0100

    chore: Lint changes

--- niall-paterson/js/main.js
+++ niall-paterson/js/main.js
@@ -12,7 +12,7 @@ window.onload = () => {
 
   document.querySelectorAll('#menuToggle, a').forEach(element => {
     element.addEventListener('click', () => {
-    toggleMenu();
+      toggleMenu();
+    });
   });
-});
 };
