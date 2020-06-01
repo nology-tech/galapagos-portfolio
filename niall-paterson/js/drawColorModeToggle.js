5c3cb8f47325f55ab45fe4e7c487a48c084b83f6
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-01 07:28:48 +0100

    refactor: Add menu toggle draw functions

---
+++ niall-paterson/js/drawColorModeToggle.js
@@ -0,0 +1,29 @@
+import { toggleGradient } from './toggleGradient.js';
+import { updateSettings } from './settings.js';
+import { toggleColorMode } from "./toggleColorMode.js";
+
+
+export const drawColorToggle = (settings) => {
+  const colorToggleSVG = SVG().addTo('#color-toggle').size(75, 75);
+  const toggleBevel = colorToggleSVG.rect(40, 20).move(30, 45).radius(10).attr({ fill: 'black' });
+  const toggleButton = colorToggleSVG.circle(16).move(32.5, 47).attr({ fill: 'white' });
+
+  colorToggleSVG.click(() => {
+    if (settings.colorMode === "dark") {
+      toggleButton.animate(300).ease("<>").move(32.5, 47);
+      
+      toggleButton.attr({ fill: 'white' });
+      toggleBevel.attr({ fill: 'black' });
+      toggleColorMode();
+      toggleGradient();
+      updateSettings();
+    } else {
+      toggleButton.animate(300).ease("<>").move(52.5, 47);
+      toggleButton.attr({ fill: 'black' });
+      toggleBevel.attr({ fill: 'white' });
+      toggleColorMode();
+      toggleGradient();
+      updateSettings();
+    }
+  });
+};
---
+++ niall-paterson/js/drawMenuToggle.js
@@ -0,0 +1,51 @@
+import { toggleMenuVisibility } from "./toggleMenu.js";
+
+
+export const animateMenu = (menuSVG) => {
+  if (settings.menuVisible) {
+    lineTop.animate(400).plot([[25, 50], [50, 50]]);
+    lineMid.animate(300, 200).plot([[25, 60], [50, 60]]);
+    lineBot.animate(400).plot([[25, 70], [50, 70]]);
+
+    toggleMenuVisibility(settings);
+
+  } else {
+    lineTop.animate(400).plot([[27.5, 70], [47.5, 50]]);
+    lineMid.animate(300).plot([[37.5, 60], [37.5, 60]]);
+    lineBot.animate(400).plot([[27.5, 50], [47.5, 70]]);
+
+    toggleMenuVisibility(settings);
+  }
+};
+
+export const drawMenuToggle = (settings) => {
+
+  const menuSVG = SVG().addTo('#menu-toggle').size(75, 75);
+  
+  const lineTop = menuSVG.line(25, 50, 50, 50);
+  const lineMid = menuSVG.line(25, 60, 50, 60);
+  const lineBot = menuSVG.line(25, 70, 50, 70);
+
+  lineTop.stroke({ color: 'black', width: 2, linecap: 'round' });
+  lineMid.stroke({ color: 'black', width: 2, linecap: 'round' });
+  lineBot.stroke({ color: 'black', width: 2, linecap: 'round' });
+
+  menuSVG.click( () => {
+    if (settings.menuVisible) {
+      lineTop.animate(400).plot([[25, 50], [50, 50]]);
+      lineMid.animate(300, 200).plot([[25, 60], [50, 60]]);
+      lineBot.animate(400).plot([[25, 70], [50, 70]]);
+
+      toggleMenuVisibility(settings);
+
+    } else {
+      lineTop.animate(400).plot([[27.5, 70], [47.5, 50]]);
+      lineMid.animate(300).plot([[37.5, 60], [37.5, 60]]);
+      lineBot.animate(400).plot([[27.5, 50], [47.5, 70]]);
+
+      toggleMenuVisibility(settings);
+    }
+  });
+};
+
+
