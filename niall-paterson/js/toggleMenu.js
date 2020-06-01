f079de4938cf73cccde0c4e26d61d56a95a941cf
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-01 17:59:00 +0100

    feat: Add menubar slider transition

--- niall-paterson/js/toggleMenu.js
+++ niall-paterson/js/toggleMenu.js
@@ -1,9 +1,11 @@
 export const toggleMenuVisibility = (settings) => {
   if (settings.menuVisible) {
-    document.querySelector('nav').style.display = 'none';
+    document.querySelector('nav').style.left = '-100vw';
+    document.querySelector('nav ul').style.opacity = '0';
     settings.menuVisible = false;
   } else {
-    document.querySelector('nav').style.display = 'block';
+    document.querySelector('nav').style.left = '0';
+    document.querySelector('nav ul').style.opacity = '1';
     settings.menuVisible = true;
   }
 };
