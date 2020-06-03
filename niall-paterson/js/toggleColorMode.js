f69d9b241bd78f8302b508a4c0c3aa84c96e14b0
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-03 21:01:31 +0100

    feat: Add grid at desktop size

--- niall-paterson/js/toggleColorMode.js
+++ niall-paterson/js/toggleColorMode.js
@@ -1,6 +1,10 @@
 import { settings } from './settings.js';
 
 export const toggleColorMode = function () {
+  
+  const toggleBevel = document.querySelector('#color-toggle > svg > rect');
+  const toggleButton = document.querySelector('#color-toggle > svg > circle');
+  const menuToggle = document.querySelectorAll('#menu-toggle > svg > line');
 
   document.querySelectorAll('*').forEach(element => {
     if (settings.colorMode === 'light') {
@@ -13,21 +17,4 @@ export const toggleColorMode = function () {
       element.style.color = '#1F1F1F';
     }
   });
-
-  const toggleBevel = document.querySelector('#color-toggle > svg > rect');
-  const toggleButton = document.querySelector('#color-toggle > svg > circle');
-  const menuToggle = document.querySelectorAll('#menu-toggle > svg > line');
-  if (settings.colorMode === 'light') {
-    toggleBevel.style.fill = '#FBFEF9';
-    toggleButton.style.fill = '#1F1F1F';
-    menuToggle.forEach(line => {
-      line.style.stroke = '#FBFEF9';
-    });
-  } else {
-    toggleBevel.style.fill = '#1F1F1F';
-    toggleButton.style.fill = '#FBFEF9';
-    menuToggle.forEach(line => {
-      line.style.stroke = '#1F1F1F';
-    });
-  }
 };
