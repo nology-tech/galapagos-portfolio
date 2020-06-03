ac9a8f500efb67be31482bc8fc81ee34219d1162
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-03 19:33:46 +0100

    refactor: Toggle colours functions

--- niall-paterson/js/drawColorModeToggle.js
+++ niall-paterson/js/drawColorModeToggle.js
@@ -1,34 +1,38 @@
 import { toggleGradient } from './toggleGradient.js';
-import { updateSettings } from './settings.js';
+import { settings, updateSettings } from './settings.js';
 import { toggleColorMode } from './toggleColorMode.js';
 
 /* global SVG */
 
-export const drawColorToggle = (settings) => {
-  const colorToggleSVG = SVG().addTo('#color-toggle').size(50, 50);
-  const toggleBevel = colorToggleSVG.rect(40, 20).center(25, 25).radius(10).attr({ fill: '#1F1F1F' });
-  const toggleButton = colorToggleSVG.circle(16).center(15, 25).attr({ fill: '#FBFEF9' });
+const colorToggleSVG = SVG().addTo('#color-toggle').size(50, 50);
+const toggleBevel = colorToggleSVG.rect(40, 20).center(25, 25).radius(10).attr({ fill: '#1F1F1F' });
+export const toggleButton = colorToggleSVG.circle(16).center(15, 25).attr({ fill: '#FBFEF9' });
 
-  colorToggleSVG.click(() => {
-    if (settings.colorMode === 'dark') {
-      toggleButton
-        .animate(100, 100, "now").size(40)
-        .animate(250, 0, "after").size(10)
-        .animate(300, 0, "after").size(16)
-        .animate(300, 0, "after").center(15, 25);
+export const toggleColours = (toggleButton, settings) => {
+  if (settings.colorMode === 'dark') {
+  toggleButton
+    .animate(100, 100, "now").size(40)
+    .animate(250, 0, "after").size(10)
+    .animate(300, 0, "after").size(16)
+    .animate(300, 0, "after").center(15, 25);
 
-      setTimeout(toggleColorMode, 100);
-      setTimeout(toggleGradient, 100);
-      setTimeout(updateSettings, 1000);
-    } else {
-      toggleButton.animate(300, 0, "now").center(35, 25)
-        .animate(100, 100, "after").size(40)
-        .animate(250, 0, "after").size(10)
-        .animate(300, 0, "after").size(16);
+  setTimeout(toggleColorMode, 100);
+  setTimeout(toggleGradient, 100);
+  setTimeout(updateSettings, 1000);
+} else {
+  toggleButton.animate(300, 0, "now").center(35, 25)
+    .animate(100, 100, "after").size(40)
+    .animate(250, 0, "after").size(10)
+    .animate(300, 0, "after").size(16);
 
-      setTimeout(toggleColorMode, 400);
-      setTimeout(toggleGradient, 400);
-      setTimeout(updateSettings, 1000);
-    }
-  });
+  setTimeout(toggleColorMode, 400);
+  setTimeout(toggleGradient, 400);
+  setTimeout(updateSettings, 1000);
+}
 };
+
+colorToggleSVG.click(() => {
+  toggleColours(toggleButton, settings);
+});
+
+
