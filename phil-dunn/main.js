4fe93c16c5ac73f87401ab395d6b5a34eaa9a4f2
phildunn81 <phillipdunn@hotmail.com>
2020-06-03 17:37:55 +0100

    linted

--- phil-dunn/main.js
+++ phil-dunn/main.js
@@ -1,8 +1,8 @@
-function sendMail() {
-  var link = `mailto:phillipdunn@hotmail.com?cc = &subject=${escape('Phil - let\'s connect')}&body=${escape('Hi Phil\n\nLet\'s connect - please reply to arrange a chat or give me a call on 07         .\n\nKind regards')}`
-    ;
-  window.location.href = link;
-}
+// function sendMail() {
+//   var link = `mailto:phillipdunn@hotmail.com?cc = &subject=${escape('Phil - let\'s connect')}&body=${escape('Hi Phil\n\nLet\'s connect - please reply to arrange a chat or give me a call on 07         .\n\nKind regards')}`
+//     ;
+//   window.location.href = link;
+// }
 
 function myFunction() {
   document.getElementById('burger').classList.toggle('change');
