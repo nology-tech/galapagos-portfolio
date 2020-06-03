d3998f92934f549840c7e5379449ba672ec71f7d
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-03 17:17:54 +0100

    refactor: Remove declarations

--- niall-paterson/js/toggleGradient.js
+++ niall-paterson/js/toggleGradient.js
@@ -1,16 +1,12 @@
 import { settings } from './settings.js';
 
-const lightGradientTop = 'linear-gradient(180deg, rgba(251, 254, 249, 1) 80%, rgba(251, 254, 249, 0.1) 100%)';
-const darkGradientTop = 'linear-gradient(180deg, rgba(31, 31, 31, 1) 80%, rgba(31, 31, 31, 0.1) 100%)';
-
 export const toggleGradient = function () {
 
   if (settings.colorMode === 'light') {
     document.querySelector('#fixed-header')
-      .style.background = darkGradientTop;
+      .style.background = 'linear-gradient(180deg, rgba(31, 31, 31, 1) 80%, rgba(31, 31, 31, 0.1) 100%)';
   } else {
-
     document.querySelector('#fixed-header')
-      .style.background = lightGradientTop;
+      .style.background = 'linear-gradient(180deg, rgba(251, 254, 249, 1) 80%, rgba(251, 254, 249, 0.1) 100%)';
   }
 };
