5695df5e53a39a712fffdffbd68d31df31b6fdc0
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-03 15:34:12 +0100

    feat: Change pure black/white to impure

--- niall-paterson/js/toggleGradient.js
+++ niall-paterson/js/toggleGradient.js
@@ -1,7 +1,7 @@
 import { settings } from './settings.js';
 
-const lightGradientTop = 'linear-gradient(180deg, rgba(255, 255, 255, 1) 80%, rgba(255, 255, 255, 0.1) 100%)';
-const darkGradientTop = 'linear-gradient(180deg, rgba(0, 0, 0, 1) 80%, rgba(0, 0, 0, 0.1) 100%)';
+const lightGradientTop = 'linear-gradient(180deg, rgba(251, 254, 249, 1) 80%, rgba(251, 254, 249, 0.1) 100%)';
+const darkGradientTop = 'linear-gradient(180deg, rgba(31, 31, 31, 1) 80%, rgba(31, 31, 31, 0.1) 100%)';
 
 export const toggleGradient = function () {
 
