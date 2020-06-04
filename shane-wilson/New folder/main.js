58ab364bb43c1ed63862ca591869eb31724622d1
shanewils <shane.wilson@nology.io>
2020-06-04 14:15:37 +0100

    lint done!

--- shane-wilson/New folder/main.js
+++
@@ -1,8 +0,0 @@
-const toggleMenu = () => {
-    let navB = document.getElementById("navb");
-    if (navB.classList.contains("show")) {
-      navB.classList.remove("show");
-    } else {
-      navB.classList.add("show");
-    }
-  }
