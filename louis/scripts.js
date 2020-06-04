20e9a2a1a775709282abc185d541b972dc5c0470
Your Name <you@example.com>
2020-06-04 15:00:52 +0100

    thursday week 3 changes

--- louis/scripts.js
+++ louis/scripts.js
@@ -1,33 +1,12 @@
 
 function dropdown() {
-  document.getElementById('dropdownLinks').classList.toggle('show');
+  document.getElementById('link1').classList.toggle('spinicon');
+  document.getElementById('link2').classList.toggle('spinicon2');
+  document.getElementById('link3').classList.toggle('spinicon3');
+  document.getElementById('link4').classList.toggle('spinicon4');
+
+  
 }
 console.log(dropdown);
 
 
-
-function spin() {
-  document.getElementById('link1').classList.toggle('spinicon');
-}
-console.log(spin);
-
-
-
-function spin2() {
-  document.getElementById('link2').classList.toggle('spinicon2');
-}
-console.log(spin2);
-
-
-
-function spin3() {
-  document.getElementById('link3').classList.toggle('spinicon3');
-}
-console.log(spin3);
-
-
-
-function spin4() {
-  document.getElementById('link4').classList.toggle('spinicon4');
-}
-console.log(spin4);
