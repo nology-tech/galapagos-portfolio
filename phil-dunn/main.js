885e727f14d1918ec3fda5d06813508c6e245c00
phildunn81 <phillipdunn@hotmail.com>
2020-06-04 17:18:19 +0100

    burger works and menu styling done

--- phil-dunn/main.js
+++ phil-dunn/main.js
@@ -2,13 +2,13 @@
 //   var link = `mailto:phillipdunn@hotmail.com?cc = &subject=${escape('Phil - let\'s connect')}&body=${escape('Hi Phil\n\nLet\'s connect - please reply to arrange a chat or give me a call on 07         .\n\nKind regards')}`
 //     ;
 //   window.location.href = link;
-// }
+// };
+
+// document.getElementsById('mail').addEventListener('click', sendMail);
 
 function myFunction() {
   document.getElementById('burger').classList.toggle('change');
-  //here
-}
-
-//document.getElementsByClassName('btn-outltine').addEventListener('click', sendMail);
+  document.querySelector('nav').classList.toggle('open');
+};
 
 document.getElementById('burger').addEventListener('click', myFunction);
