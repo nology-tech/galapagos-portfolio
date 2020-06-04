85b1f0a00e1b5b664857fd794e0d1ca4a34ae2f5
Your Name <you@example.com>
2020-06-04 15:20:40 +0100

    linting fixed

--- louis/scripts.js
+++ louis/scripts.js
@@ -4,9 +4,8 @@ function dropdown() {
   document.getElementById('link2').classList.toggle('spinicon2');
   document.getElementById('link3').classList.toggle('spinicon3');
   document.getElementById('link4').classList.toggle('spinicon4');
-
-  
 }
+
 console.log(dropdown);
 
 
