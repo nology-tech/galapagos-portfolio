1a44d0c1567c6d7e365095d54c328c9f3f99af70
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-04 15:33:54 +0100

    refactor: Use function chaining for color toggle

--- niall-paterson/js/drawColorModeToggle.js
+++ niall-paterson/js/drawColorModeToggle.js
@@ -2,17 +2,38 @@ import { toggleGradient } from './toggleGradient.js';
 import { updateSettings } from './settings.js';
 import { toggleColorMode } from './toggleColorMode.js';
 
-/* global SVG */
-
-export const drawColorToggle = (settings) => {
-  
-  const colorToggleSVG = SVG().addTo('#color-toggle').size(50, 50);
-  const toggleBevel = colorToggleSVG.rect(40, 20).center(25, 25).radius(10).attr({ fill: '#1F1F1F' });
-  const toggleButton = colorToggleSVG.circle(16).center(15, 25).attr({ fill: '#FBFEF9' });
-
-  const toggleColours = (toggleButton, settings) => {
+export const toggleButton = {
+  svgFrame: undefined,
+  bevel: undefined,
+  button: undefined,
+  drawFrame() {
+    this.svgFrame = SVG().addTo('#color-toggle').size(50, 50);
+    return this;
+  },
+  drawBevel() {
+    this.bevel = this.svgFrame
+      .rect(40, 20)
+      .center(25, 25)
+      .radius(10)
+      .attr({ fill: '#1F1F1F' });
+    return this;
+  },
+  drawButton() {
+    this.button = this.svgFrame
+      .circle(16)
+      .center(15, 25)
+      .attr({ fill: '#FBFEF9' });
+    return this;
+  },
+  addAnimateClickListener(settings) {
+  this.svgFrame.click(() => {
+    this.runAnimation(settings);
+  });
+  return this;
+  },
+  runAnimation(settings) {
     if (settings.colorMode === 'dark') {
-      toggleButton
+      this.button
         .animate(100, 100, "now").size(40)
         .animate(250, 0, "after").size(10)
         .animate(300, 0, "after").size(16)
@@ -22,7 +43,7 @@ export const drawColorToggle = (settings) => {
       setTimeout(toggleGradient, 100);
       setTimeout(updateSettings, 1000);
     } else {
-      toggleButton.animate(300, 0, "now").center(35, 25)
+      this.button.animate(300, 0, "now").center(35, 25)
         .animate(100, 100, "after").size(40)
         .animate(250, 0, "after").size(10)
         .animate(300, 0, "after").size(16);
@@ -31,10 +52,6 @@ export const drawColorToggle = (settings) => {
       setTimeout(toggleGradient, 400);
       setTimeout(updateSettings, 1000);
     }
-  };
-
-  colorToggleSVG.click(() => {
-    toggleColours(toggleButton, settings);
-  });
+    return this;
+  },
 };
-
