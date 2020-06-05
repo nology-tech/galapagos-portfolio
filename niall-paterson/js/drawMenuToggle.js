31f3639143fe8ba52da7f007eb9522aec41d7acf
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-05 09:58:31 +0100

    refactor: Use method chaining for menu toggle

--- niall-paterson/js/drawMenuToggle.js
+++ niall-paterson/js/drawMenuToggle.js
@@ -2,49 +2,66 @@ import { toggleMenuVisibility } from './toggleMenu.js';
 
 /* global SVG */
 
-export const drawMenuToggle = (settings) => {
-
-  const menuSVG = SVG().addTo('#menu-toggle').size(50, 50);
-
-  const lineTop = menuSVG.line(10, 20, 40, 20);
-  const lineBot = menuSVG.line(10, 35, 40, 35);
-
-  lineTop.stroke({ color: '#1F1F1F', width: 2, linecap: 'round' });
-  lineBot.stroke({ color: '#1F1F1F', width: 2, linecap: 'round' });
-
-  menuSVG.click( () => {
-    animateMenu();
-  });
-
-  const animateMenu = () => {
+export const toggleMenu = {
+  svgFrame: undefined,
+  lineTop: undefined,
+  lintBot: undefined,
+  drawFrame() {
+    this.svgFrame = SVG().addTo('#menu-toggle').size(50, 50);
+    return this;
+  },
+  drawLineTop() {
+    this.lineTop = this.svgFrame
+      .line(10, 20, 40, 20);
+    
+    this.lineTop.stroke(
+      { color: '#1F1F1F', width: 2, linecap: 'round' }
+    );
+    return this;
+  },
+  drawLineBot() {
+    this.lineBot = this.svgFrame
+      .line(10, 35, 40, 35);
+      
+    this.lineBot.stroke(
+      { color: '#1F1F1F', width: 2, linecap: 'round' }
+    );
+    return this;
+  },
+  addAnimateClickListeners(settings) {
+    // animate menu if click menu
+    this.svgFrame.click(() => {
+      this.runAnimation(settings);
+      toggleMenuVisibility(settings);
+    });
+    // animate menu if click nav links
+    document.querySelectorAll('a').forEach(element => {
+      element.addEventListener('click', () => {
+        toggleMenuVisibility(settings);
+        toggleMenu.runAnimation(settings);
+      });
+    });
+    return this;
+  },
+  runAnimation(settings) {
     if (settings.menuVisible) {
-      lineTop.
-        animate(400, 0, 'now').plot([[12, 15], [38, 27]])
+      this.lineTop
+        .animate(400, 0, 'now').plot([[12, 15], [38, 27]])
         .animate(1000, 100, 'after').plot([[10, 20], [40, 20]]);
-
-      lineBot
+      this.lineBot
         .animate(400, 100, 'now').plot([[38, 20], [12, 35]])
         .animate(1000, 100, 'after').plot([[10, 35], [40, 35]]);
-
-      toggleMenuVisibility(settings);
-
     } else {
-      lineTop
+      this.lineTop
         .animate(400, 0, 'now').plot([[12, 15], [38, 27]])
         .animate(1000, 100, 'after').plot([[37.5, 12.5], [12.5, 37.5]]);
-
-      lineBot
+      this.lineBot
         .animate(400, 0, 'now').plot([[38, 20], [12, 35]])
         .animate(1000, 100, 'after').plot([[12.5, 12.5], [37, 37.5]]);
-
-      toggleMenuVisibility(settings);
     }
-  };
-
-  document.querySelectorAll('a').forEach(element => {
-    element.addEventListener('click', () => {
-      toggleMenuVisibility(settings);
-      animateMenu();
-    });
-  });
+    return this;
+  }
 };
+
+
+  
