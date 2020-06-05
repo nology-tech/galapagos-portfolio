2ea9d35dba737a505a9a259c6e884652a0b8e8e9
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-05 09:59:29 +0100

    style: Remove unnecessary import; reorder imports

--- niall-paterson/js/main.js
+++ niall-paterson/js/main.js
@@ -1,7 +1,6 @@
-// import { drawColorToggle } from './drawColorModeToggle.js';
 import { toggleMenu } from './drawMenuToggle.js';
-import { settings } from './settings.js';
 import { toggleButton } from './drawColorModeToggle.js';
+import { settings } from './settings.js';
 
 window.onload = () => {
   toggleButton.drawFrame().drawBevel().drawButton().addAnimateClickListener(settings);
