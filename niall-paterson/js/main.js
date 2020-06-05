58f6dcea203737a0a2f2871cff46620d8ca6e31c
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-05 09:58:31 +0100

    refactor: Use method chaining for menu toggle

--- niall-paterson/js/main.js
+++ niall-paterson/js/main.js
@@ -1,18 +1,9 @@
 // import { drawColorToggle } from './drawColorModeToggle.js';
-import { drawMenuToggle } from './drawMenuToggle.js';
+import { toggleMenu } from './drawMenuToggle.js';
 import { settings } from './settings.js';
-import { toggleMenuVisibility } from './toggleMenu.js';
 import { toggleButton } from './drawColorModeToggle.js';
 
 window.onload = () => {
-
-  drawMenuToggle(settings);
-  
   toggleButton.drawFrame().drawBevel().drawButton().addAnimateClickListener(settings);
-  
-  document.querySelectorAll('a').forEach(element => {
-    element.addEventListener('click', () => {
-      toggleMenuVisibility(settings);
-    });
-  });
+  toggleMenu.drawFrame().drawLineTop().drawLineBot().addAnimateClickListeners(settings);
 };
