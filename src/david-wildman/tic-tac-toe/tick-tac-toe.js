241b628cb759d3f45d28fe177ba743bf550282bb
David Wildman <59884105+TheWildebeest@users.noreply.github.com>
2020-06-09 19:30:42 +0100

    These messages are getting unmanageably long. I cannot keep up with what I have started.
    It was going to be so epic. The story of how I coded my first JavaScript game,
    told in a series of excessively eloquent commit messages. But I can't
    keep this up. I'm spending so much time writing the story that I am rapidly running
    out of time to actually code the game...
    
    ...thought David to himself as he began writing his next commit message.
    
    But he was a sucker for a blank canvas, and as the words spilled onto
    the console, he couldn't help but continue the telling of the story
    he had only seconds earlier sworn off.
    
    David was fairly please with himself, all things considered. He had refactored
    his objects, formed from two classes which were extended from a base class,
    into an array of objects which was now being imported from a separate file.
    
    I am not doing this to impress anyone
    
    'Goodness' he thought to himself, 'I need to stop pressing the
    up and down arrows. They mess everything up'Someone should make a name for this sensation', he thought to himself, blissfully unaware of the wall which he was rapidly approaching.
    
    I decided it was time to end this facade and admit I had no idea what was going on

---
+++ src/david-wildman/tic-tac-toe/tick-tac-toe.js
@@ -0,0 +1,56 @@
+console.log("HELLO");
+let turn = 0;
+
+// CLICK EVENTS //
+
+// Looping over the squares to add an event listener //
+// Clicking on every square adds  either a 0 or an X //
+
+// every = need a loop over all the squares //
+
+// If a box is clicked //
+  // If a box has been clicked //
+    // if turn is 2, 4 or 6, add X otherwise 0 //
+
+
+// Two users, X and 0, and their moves //
+const addEventListeners = () => {
+  const squares = document.getElementsByClassName("square");
+  squares.forEach(square => {
+    square.addEventListener("click", () => {
+
+      if (square.innerHTML === "") {
+        if (turn % 2 === 0) {
+          square.innerHTML = "X";
+        } else {
+          square.innerHTML = "0";
+        }
+      }
+      turn++;
+    });
+  });
+};
+
+
+const squares = document.querySelectorAll(".square");
+
+console.log(squares);
+
+squares.forEach((square) => {
+  square.addEventListener("click", () => {
+    console.log("click");
+  });
+});
+
+
+// Turn counter //
+
+// 2. WIN LOGIC //
+  // If there are three in line - | / \ current user has won //
+  // add one to the current user //
+
+
+
+
+// 3. RESET //
+  // Trigger to start the whole game //
