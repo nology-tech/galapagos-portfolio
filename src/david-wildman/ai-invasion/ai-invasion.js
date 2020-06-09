a63d5eed4f309edc12d9cf5b488a69a11b13c951
David Wildman <59884105+TheWildebeest@users.noreply.github.com>
2020-06-09 19:30:42 +0100

    These messages are getting unmanageably long. I cannot keep up with what I have started.
    It was going to be so epic. The story of how I coded my first JavaScript game,
    told in a series of excessively eloquent commit messages. But I can't
    keep this up. I'm spending so much time writing the story that I am rapidly running
    out of time to actually code the game...
    
    ...thought David to himself as he began writing his next commit message.
    
    But he was a sucker for a blank canvas, and as the words spilled onto
    the console, he couldn't help but continue the telling of the story
    he had only seconds earlier sworn off.
    
    David was fairly please with himself, all things considered. He had refactored
    his objects, formed from two classes which were extended from a base class,
    into an array of objects which was now being imported from a separate file.
    
    I am not doing this to impress anyone
    
    'Goodness' he thought to himself, 'I need to stop pressing the
    up and down arrows. They mess everything up'Someone should make a name for this sensation', he thought to himself, blissfully unaware of the wall which he was rapidly approaching.
    
    I decided it was time to end this facade and admit I had no idea what was going on

--- src/david-wildman/ai-invasion/ai-invasion.js
+++ src/david-wildman/ai-invasion/ai-invasion.js
@@ -1,10 +1,15 @@
+import  { wordsArray } from "./modules/ai-invasion-names.js";
+
+for ( let i = 0; i < wordsArray.length; i ++) {
+}
+
 console.log("AI Invasion v0.1");
 
 /* ---------- AI INVASION - a game by David Wildman ---------*/
 
 
 /* ---------- The aim of this game is to review words that pop up on your screen, and shoot any that are not real words.
--------------
+------------- 
 ------------- When the game starts, the will see a "play" button and an "instructions" button.
 ------------- On clicking "instructions", a guide to playing the game is shown.
 ------------- On clicking "play",  countdown begins from 3.
@@ -26,7 +31,7 @@ console.log("AI Invasion v0.1");
 
 // Starting and pausing the game //
 
-let gamePaused = true;
+let gameReset = true;
 
 // Game variables //
 
@@ -36,110 +41,41 @@ let scoreTwo = 0;
 let scoreThree = 0;
 let highScore = 0;
 let currentLanguagePollutionLevel = 0;
-const currentGameWords = [];
 
 // DOM Elements //
 
-const pauseToggleButton = document.querySelector("#pause-toggle-button");
+const resetToggleButton = document.querySelector("#reset-toggle-button");
+const gameBoard = document.querySelector(".game-board");
 
 
 
 /*----------- GLOBAL FUNCTIONS ----------*/
 
-// Play and pause button //
+// Play and reset button //
 
-const togglePause = () => {
-  if (gamePaused === true) {
-    gamePaused = false;
-    console.log("Game unpaused.");
-    pauseToggleButton.setAttribute("value", "PAUSE")
+const toggleReset = () => {
+  if (gameReset === true) {
+    gameReset = false;
+    console.log("Game started.");
+    resetToggleButton.setAttribute("value", "NEW GAME")
   } else {
-    gamePaused = true;
-    console.log("Game paused.");
-    pauseToggleButton.setAttribute("value", "PLAY")
+    gameReset = true;
+    console.log("Game reset.");
+    resetToggleButton.setAttribute("value", "PLAY")
   }
 }
 
 // Random number generator, used for element coordinates // 
 
-const getRandomNumber = () => {
-  return Math.floor(Math.random() * 100) + 1;
-}
-
 /*----------- ------------------------------------------------------------------------------------------------------------*/
 
 /*----------- GLOBAL CLASSES ----------*/
-
-// Word object template //
-
-class Word {
-  constructor(word, definition, isReal) { 
-    this.word = word;
-    this.definition = definition;
-    this.isReal = isReal;
-    this.pointsScore = this.word.length;
-    this.XPosition = getRandomNumber()+"%";
-    this.YPosition = getRandomNumber()+"%";
-    currentGameWords.push(this);
-  }
-  giveOverview() {
-    if (this.isReal === true) {
-    return `${this.word}. Real word. Definition: ${this.definition}.`
-    } else if (this.isReal === false) {
-    return `${this.word}. Fake word. Definition: ${this.definition}.`
-    } else {
-      return "This word has an invalid 'isReal' value. Please send a strongly-worded email to david.wildman@nology.io."
-    };
-  }
-}
-
-// Fake Word object template //
-
-class FakeWord extends Word {
-  constructor(word, definition) { 
-    super(word, definition, false)
-  }
-}
-
-class RealWord extends Word {
-  constructor(word, definition) { 
-    super(word, definition, true)
-  }
-}
-
-/*----------- GLOBAL OBJECTS ----------*/
-
-// Fake words //
-
-const covfefe = new FakeWord("covfefe", "noun. widespread negative media coverage.");
-const carnacé = new FakeWord("carnacé", "noun. a leather shoe or shoelace.");
-const nonindicating = new FakeWord("nonindicating", "adjective. not officially attesting to an opinion or statement.");
-const wreatheness = new FakeWord("wreatheness", "noun. a quality or fact that is the product of time, often thought of as heritable.");
-const conturbulent = new FakeWord("conturbulent", "noun. (of speech, writing, or writing styles) shaped into straight lines.");
-const spadazone = new FakeWord("spadazone", "noun. a synthetic compound of amygdalin that is one of the chief active constituents of theophylline");
-const triscule = new FakeWord("triscule", "adjective. small or barely visible");
-const poandie = new FakeWord("poandie", "noun. an angry, aggressive person");
-const teruur = new FakeWord("teruur", "noun. a native or inhabitant of the former Spanish kingdom of Veracruz");
-const decalcop = new FakeWord("decalcop", "a small structure in a thorax of leaflets");
-
-
-// Real words //
-
-const octothorpe = new RealWord("octothorpe", "noun. the 'hashtag' (#) symbol.");
-const agelast = new RealWord("agelast", "noun. a person who never laughs.");
-const peristeronic = new RealWord("peristeronic", "of or relating to pigeons");
-const biblioklept = new RealWord("biblioklept", "noun. one who steals books.");
-const acnestis = new RealWord("acnestis", "noun. the part of the back which an animal cannot reach to scratch.");
-const hircine = new RealWord("hircine", "ajective. of, relating to, or suggestive of a goat.");
-const tumescent = new RealWord("tumescent", "adjective. somewhat swollen.");
-const dissimulate = new RealWord("dissimulate", "verb. to hide under a false appearance");
-const sententious = new RealWord("sententious", "adjective. given to or abounding in excessive moralizing.");
-const chimerical = new RealWord("chimerical", "adjective. existing only as the product of unchecked imagination");
 
 
 
 
 // Event Listeners //
 
-pauseToggleButton.addEventListener("click", togglePause);
-currentGameWords.forEach((word) => console.log(word));
+resetToggleButton.addEventListener("click", toggleReset);
+wordsArray.forEach((wordObject) => console.log(wordObject.word));
+
---
+++ src/david-wildman/ai-invasion/modules/ai-invasion-names.js
@@ -0,0 +1,92 @@
+// Word object template //
+
+
+
+export class Word {
+  constructor(word, definition, isReal, getRandomNumber) {
+    getRandomNumber = () => Math.floor(Math.random() * 100) + 1;
+    this.word = word;
+    this.definition = definition;
+    this.isReal = isReal;
+    this.pointsScore = this.word.length;
+    this.XPosition = getRandomNumber()+"%";
+    this.YPosition = getRandomNumber()+"%";
+  }
+  giveOverview() {
+    if (this.isReal === true) {
+    return `${this.word}. Real word. Definition: ${this.definition}.`
+    } else if (this.isReal === false) {
+    return `${this.word}. Fake word. Definition: ${this.definition}.`
+    } else {
+      return "This word has an invalid 'isReal' value. Please send a strongly-worded email to david.wildman@nology.io."
+    };
+  }
+}
+
+// Fake Word object template //
+
+export class FakeWord extends Word {
+  constructor(word, definition) { 
+    super(word, definition, false)
+  }
+}
+
+// Real Word object template //
+
+export class RealWord extends Word {
+  constructor(word, definition) { 
+    super(word, definition, true)
+  }
+}
+
+/*----------- GLOBAL OBJECTS ----------*/
+
+// Fake words //
+
+const fakeWord1 = new FakeWord("covfefe", "noun. widespread negative media coverage.");
+const fakeWord2 = new FakeWord("carnacé", "noun. a leather shoe or shoelace.");
+const fakeWord3 = new FakeWord("nonindicating", "adjective. not officially attesting to an opinion or statement.");
+const fakeWord4 = new FakeWord("wreatheness", "noun. a quality or fact that is the product of time, often thought of as heritable.");
+const fakeWord5 = new FakeWord("conturbulent", "noun. (of speech, writing, or writing styles) shaped into straight lines.");
+const fakeWord6 = new FakeWord("spadazone", "noun. a synthetic compound of amygdalin that is one of the chief active constituents of theophylline");
+const fakeWord7 = new FakeWord("triscule", "adjective. small or barely visible");
+const fakeWord8 = new FakeWord("poandie", "noun. an angry, aggressive person");
+const fakeWord9 = new FakeWord("teruur", "noun. a native or inhabitant of the former Spanish kingdom of Veracruz");
+const fakeWord10 = new FakeWord("decalcop", "a small structure in a thorax of leaflets");
+
+
+// Real words //
+
+const realWord1 = new RealWord("octothorpe", "noun. the 'hashtag' (#) symbol.");
+const realWord2 = new RealWord("agelast", "noun. a person who never laughs.");
+const realWord3 = new RealWord("peristeronic", "of or relating to pigeons");
+const realWord4 = new RealWord("biblioklept", "noun. one who steals books.");
+const realWord5 = new RealWord("acnestis", "noun. the part of the back which an animal cannot reach to scratch.");
+const realWord6 = new RealWord("hircine", "ajective. of, relating to, or suggestive of a goat.");
+const realWord7 = new RealWord("tumescent", "adjective. somewhat swollen.");
+const realWord8 = new RealWord("dissimulate", "verb. to hide under a false appearance");
+const realWord9 = new RealWord("sententious", "adjective. given to or abounding in excessive moralizing.");
+const realWord10 = new RealWord("chimerical", "adjective. existing only as the product of unchecked imagination");
+
+export const wordsArray = [
+  fakeWord1,
+  fakeWord2,
+  fakeWord3,
+  fakeWord4,
+  fakeWord5,
+  fakeWord6,
+  fakeWord7,
+  fakeWord8,
+  fakeWord9,
+  fakeWord10,
+  realWord1,
+  realWord2,
+  realWord3,
+  realWord4,
+  realWord5,
+  realWord6,
+  realWord7,
+  realWord8,
+  realWord9,
+  realWord10
+]
