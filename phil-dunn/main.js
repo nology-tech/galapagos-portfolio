fb28014394af75b13278150e4c9a236d7b821753
phildunn81 <phillipdunn@hotmail.com>
2020-06-09 15:02:07 +0100

    icons added, fixed email

--- phil-dunn/main.js
+++ phil-dunn/main.js
@@ -1,10 +1,3 @@
-// function sendMail() {
-//   var link = `mailto:phillipdunn@hotmail.com?cc = &subject=${escape('Phil - let\'s connect')}&body=${escape('Hi Phil\n\nLet\'s connect - please reply to arrange a chat or give me a call on 07         .\n\nKind regards')}`
-//     ;
-//   window.location.href = link;
-// };
-
-// document.getElementsById('mail').addEventListener('click', sendMail);
 
 function myFunction() {
   document.getElementById('burger').classList.toggle('change');
@@ -12,3 +5,14 @@ function myFunction() {
 }
 
 document.getElementById('burger').addEventListener('click', myFunction);
+
+document.getElementById('mail').addEventListener('click', sendMail);
+
+function sendMail() {
+  let link ='mailto:phillipdunn@hotmail.com'
++ '?cc='
++ '&subject=' + escape('Phil - let\'s connect')
++ '&body=' + escape('Hi Phil\n\nLet\'s connect - please reply to arrange a chat or give me a call on 07         .\n\nKind regards');
+  window.location.href = link;
+}
+
