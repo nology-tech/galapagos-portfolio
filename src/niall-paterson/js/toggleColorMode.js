1abe32de9c8b92e0119a78eaec0a6f176ce48ac7
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2020-06-14 11:57:39 +0100

    improve: Replace color toggle with class system

--- src/niall-paterson/js/toggleColorMode.js
+++ src/niall-paterson/js/toggleColorMode.js
@@ -1,32 +1,53 @@
 import { settings } from './settings.js';
+import { toggleButton } from './drawColorModeToggle.js';
+import { toggleMenu } from './drawMenuToggle.js';
 
-export const toggleColorMode = function () {
-  // all elements
-  document.querySelectorAll('*').forEach(element => {
-    if (settings.colorMode === 'light') {
-      element.style.backgroundColor = '#1F1F1F';
-      element.style.fill = '#FBFEF9';
-      element.style.color = '#FBFEF9';
-      element.style.stroke = '#FBFEF9'; // for line elements
-    } else {
-      element.style.backgroundColor = '';
-      element.style.fill = '#1F1F1F';
-      element.style.color = '#1F1F1F';
-      element.style.stroke = '#1F1F1F'; // ditto
+export const toggleColorMode = () => {
+
+  console.log(toggleButton);
+  // we do backgrounds and colors separately, since some
+  // elements have transparent backgrounds
+
+  document.querySelectorAll('*').forEach((element) => {
+    if (element.classList.contains('lightModeText')) {
+      element.classList.remove('lightModeText');
+      element.classList.add('darkModeText');
+    } else if (element.classList.contains('darkModeText')) {
+      element.classList.remove('darkModeText');
+      element.classList.add('lightModeText');
     }
+
+    if (element.classList.contains('lightModeBackground')) {
+      element.classList.remove('lightModeBackground');
+      element.classList.add('darkModeBackground');
+    } else if (element.classList.contains('darkModeBackground')){
+      element.classList.remove('darkModeBackground');
+      element.classList.add('lightModeBackground');
+    }
+
+    if (element.classList.contains('lightModeBoxShadow')) {
+      element.classList.remove('lightModeBoxShadow');
+      element.classList.add('darkModeBoxShadow');
+    } else if (element.classList.contains('darkModeBoxShadow')){
+      element.classList.remove('darkModeBoxShadow');
+      element.classList.add('lightModeBoxShadow');
+    }
+
   });
 
-  // svg button element
-  const toggleButton = document.querySelector('#color-toggle > svg > circle');
-  settings.colorMode === 'light' ? toggleButton.style.fill = '#1F1F1F' : toggleButton.style.fill = '#FBFEF9';
 
-  // project card border shadows
-  const projectCards = document.querySelectorAll('.project-wrapper');
-  projectCards.forEach(card => {
-    if (settings.colorMode === 'light') {
-      card.style.boxShadow = '0 0 16px -3px rgba(251, 254, 249, 0.19)'; 
-    } else {
-      card.style.boxShadow = '0 0 16px -3px rgba(31, 31, 31, 0.19)';
-    }
-  });
+  // svgs
+
+  if (settings.colorMode === 'light') {
+    toggleButton.bevel.fill('#fbfef9');
+    toggleButton.button.fill('#1f1f1f').stroke('#fbfef9');
+    toggleMenu.lineTop.stroke('#fbfef9');
+    toggleMenu.lineBot.stroke('#fbfef9');
+  } else {
+    toggleButton.bevel.fill('#1f1f1f');
+    toggleButton.button.fill('#fbfef9').stroke('#1f1f1f');
+    toggleMenu.lineTop.stroke('#1f1f1f');
+    toggleMenu.lineBot.stroke('#1f1f1f');
+  }
+
 };
