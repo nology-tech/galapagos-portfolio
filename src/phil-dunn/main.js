ad896fc93262364d5ac37b9d659ca9e993ae5fb0
phildunn81 <phillipdunn@hotmail.com>
2020-06-14 15:52:35 +0100

    added project images

--- src/phil-dunn/main.js
+++ src/phil-dunn/main.js
@@ -1,18 +1,16 @@
-
-function myFunction() {
+const menu = () => {
   document.getElementById('burger').classList.toggle('change');
   document.querySelector('nav').classList.toggle('open');
-}
+};
 
-document.getElementById('burger').addEventListener('click', myFunction);
 
-document.getElementById('mail').addEventListener('click', sendMail);
-
-function sendMail() {
+const sendMail =() => {
   let link ='mailto:phillipdunn@hotmail.com'
 + '?cc='
 + '&subject=' + escape('Phil - let\'s connect')
 + '&body=' + escape('Hi Phil\n\nLet\'s connect - please reply to arrange a chat or give me a call on 07         .\n\nKind regards');
   window.location.href = link;
-}
+};
 
+document.getElementById('burger').addEventListener('click', menu);
+document.getElementById('mail').addEventListener('click', sendMail);
