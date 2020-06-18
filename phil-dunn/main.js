6f79621a49cb5cd32b629aa594742873e7b154ea
mph206 <64665531+mph206@users.noreply.github.com>
2020-06-18 16:25:09 +0000

    Deploying to gh-pages from  @ 5ca61cdeb0fe5af0d08cffbeda305369cb60f0cd 🚀

--- phil-dunn/main.js
+++ phil-dunn/main.js
@@ -5,9 +5,10 @@ const menu = () => {
 
 const sendMail =() => {
   let link ='mailto:phillipdunn@hotmail.com'
-+ '?cc='
-+ '&subject=' + escape('Phil - let\'s connect')
-+ '&body=' + escape('Hi Phil\n\nLet\'s connect - please reply to arrange a chat or give me a call on 07         .\n\nKind regards');
++ '?cc= ' 
+  + '&subject=' + escape('Phil - let\'s connect')
+  + '&body=' + escape('Hi Phil\n\nLet\'s connect - please reply to arrange a chat or give me a call on\n\nKind regards')
+  ;
   window.location.href = link;
 };
 
