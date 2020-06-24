2c9817f371c962568388cf9964dc41bfd4266379
David Wildman <59884105+TheWildebeest@users.noreply.github.com>
2020-06-25 00:34:15 +0100

    I really like how this is coming along. I've set up an enter button which is the only thing you see when you first arrive at the page. On clicking, it turns blue before disappearing and being replaced by the main menu. If I can get the main menu elements to fade or slide in, I'll be pretty much done with the javascript for this site and can finally start adding content.

--- src/david-wildman/assets/app.js
+++ src/david-wildman/assets/app.js
@@ -1,3 +1,6 @@
+const enterButton = document.querySelector('#enter-button');
+const landingPage = document.querySelector('.landing.page');
+const homePage = document.querySelector('.home.page');
 const header = document.querySelector('header');
 const headerTitleContainer = document.querySelector(
   '.header-title.landing-page-effect'
@@ -6,23 +9,32 @@ const headerTitleOne = document.querySelector('.title-one');
 const headerTitleTwo = document.querySelector('.title-two');
 const headerContents = document.querySelector('header *');
 const navButtons = document.querySelector('body > header > nav > ul');
-const enterButton = document.querySelector('#enter-button');
+
 const bioButton = document.querySelector('#bio-button');
 const portfolioButton = document.querySelector('#portfolio-button');
 const contactButton = document.querySelector('#contact-button');
 
 // const foldAway = () => {
 //   console.log('heyyyy');
-// };
+// };.
+
 
 const mainMenu = () => {
-  enterButton.style.display = 'none';
+  enterButton.style.backgroundImage = "url('./assets/images/downwardsbackgroundmingblue.gif')";
+  enterButton.style.borderColor = "#8ea8c3"
+  enterButton.style.color = "#8ea8c3"
+  setTimeout(() => {
+    console.log(landingPage.classList);
+    landingPage.classList.remove('on');
+    homePage.classList.add('on');
+  }, 2000);;
   headerTitleContainer.classList.toggle('landing-page-effect');
   headerTitleOne.classList.toggle('landing-page-effect');
   headerTitleTwo.classList.toggle('landing-page-effect');
 };
 
 // navButtons.addEventListener('click', foldAway);
+enterButton.addEventListener('click', mainMenu);
 headerTitleContainer.addEventListener('click', mainMenu);
 // bioButton.addEventListener('click', foldAway);
 // portfolioButton.addEventListener('click', foldAway);
