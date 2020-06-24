f0c82c4c09ff5ff2a4c16e4a05ebece6a3af0312
David Wildman <59884105+TheWildebeest@users.noreply.github.com>
2020-06-24 23:29:56 +0100

    making a commit of this stable version before I mess everything up!

--- src/david-wildman/assets/app.js
+++ src/david-wildman/assets/app.js
@@ -6,6 +6,7 @@ const headerTitleOne = document.querySelector('.title-one');
 const headerTitleTwo = document.querySelector('.title-two');
 const headerContents = document.querySelector('header *');
 const navButtons = document.querySelector('body > header > nav > ul');
+const enterButton = document.querySelector('#enter-button');
 const bioButton = document.querySelector('#bio-button');
 const portfolioButton = document.querySelector('#portfolio-button');
 const contactButton = document.querySelector('#contact-button');
@@ -15,6 +16,7 @@ const contactButton = document.querySelector('#contact-button');
 // };
 
 const mainMenu = () => {
+  enterButton.style.display = 'none';
   headerTitleContainer.classList.toggle('landing-page-effect');
   headerTitleOne.classList.toggle('landing-page-effect');
   headerTitleTwo.classList.toggle('landing-page-effect');
