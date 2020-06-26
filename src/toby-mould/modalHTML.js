3a8cabccb65341c145b6152cee43524f5a522242
tobymould <toby.mould@icloud.com>
2020-06-26 18:35:39 +0100

    fixed latest version with data object injection of content (but modal POP-OUT styling not working)

---
+++ src/toby-mould/modalHTML.js
@@ -0,0 +1,106 @@
+import * as data from './data.js';
+
+const modalContent = document.querySelector('#simpleModal .modal-content');
+const modalStructure = document.querySelector('#simpleModal');
+
+// let modalhtml =
+
+export const something = projectNumber => {
+  console.log(`It Reached inside the SOMETHING FUNCTION`);
+  // console.log(data);
+  const project = data.data[projectNumber];
+  // console.log(projects.difficult);
+  const modalhtml = `
+  <span class="closeBtn">&times;</span>
+  <!-- Modal Content on Project -->
+    <article id="project1" class="text-justify">
+  
+    <!-- Column-1 -->
+      <div id="column-1">
+  
+        <!-- Summary Section -->
+        <div id="summary">
+          <h3>${project.name}</h3>
+          <div id="project1Image">
+            <h4>Live Preview</h4>
+          </div>
+          <div class="buttons">
+            <i class="fas fa-desktop"
+              ><a
+                class="btn"
+                href="${project.live}"
+                target="_blank"
+                >Live Preview</a
+              ></i
+            >
+            <i class="fas fa-code-branch"
+              ><a
+                class="btn"
+                href="${project.github}"
+                target="_blank"
+                >Source Private</a
+              ></i
+            >
+          </div>
+          <p>
+          ${project.overview}
+          </p>
+        </div>
+  
+        <!-- Technology Section -->
+        <div id="tech">
+          <h4>Technologies Used</h4>
+          <div class="table">
+            <ul class="fa-ul">
+              ${project.technology}
+            </ul>
+          </div>
+        </div>
+      </div>
+  
+      <!-- Column 2 -->
+      <div id="column-2">
+  
+        <!-- Role Content -->
+        <div id="role">
+          <h4>My Role</h4>
+          <p>
+          ${project.role}
+          </p>
+        </div>
+  
+        <!-- Difficulties Content -->
+        <div id="diff">
+          <h4>Project Difficulties</h4>
+          <p>
+          ${project.difficult}
+          </p>
+        </div>
+  
+        <!-- Solution Content -->
+        <div id="sol">
+          <h4>My Solution</h4>
+          <p>
+          ${project.solution}
+          </p>
+        </div>
+  
+        <!-- Notable Features Content -->
+        <div id="feat">
+          <h4>Notable Features</h4>
+          <ul>
+          ${project.features}
+          </ul>
+        </div>
+      </div>
+    </article>`;
+  //
+  modalContent.innerHTML = modalhtml;
+  console.log(modalContent);
+  // modalStructure.style.display = 'block';
+
+  // projects.forEach(project => {
+  //   //
+  //   // console.log('hello');
+  // });
+};
