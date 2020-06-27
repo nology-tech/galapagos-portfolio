9514301870cfc1de814ee270627c77cc1bc5e9ea
tobymould <toby.mould@icloud.com>
2020-06-27 12:10:29 +0100

    fixed modal pop-up on the page, the actual display of the modal content style, and the the event listener to close the modal afterward.

--- src/toby-mould/app.js
+++ src/toby-mould/app.js
@@ -1,58 +1,52 @@
-import * as modalhtml from './modalHTML.js';
-import * as data from './data.js';
+import * as modalFunctionFile from './modalHTML.js';
+import * as dataFile from './data.js';
 
 // 1. Grab Items
 const portfolioGrid = document.querySelector('#portfolio .items');
 const portfolioItems = document.querySelectorAll('#portfolio .item');
 const modalContent = document.querySelector('#simpleModal .modal-content');
-const modalStructure = document.querySelector('#simpleModal');
+
 console.log(portfolioGrid);
 const projectList = document.querySelector('#portfolio .items');
 // ----------------------------------------------------------------
-
-const buttons = data.data.map((project, index) => {
+// STEP 1 - AUTO-RENDER THE PROJECT BUTTONS USING 'DATA.JS' FILE.
+const buttons = dataFile.data.map((project, index) => {
   return `
   <button class="item" value="${index}">
     <h4>${project.name}</h4>
   </button>`;
 });
-projectList.innerHTML = buttons.join('');
+projectList.innerHTML += buttons.join('');
+// projectList.innerHTML +=
 
+// STEP 2 - WHEN PROJECT BUTTON PRESSED, RUN THE MODAL DISPLAY FUNCTION TAKING IN THE BUTTON NUMBER PRESSED AS AN ARG.
 // 3. Event Handler
-const modalEventHandler = event => {
+const modalOpen = event => {
   const buttonClicked = event.target.value;
   console.log(`it reached inside Event Handler - ${buttonClicked}`);
-
   switch (buttonClicked) {
     case '0':
-      modalhtml.something(0);
       console.log(`it reached inside SWITCH - ${buttonClicked}`);
+      modalFunctionFile.insertProjectModalFunction(0);
       break;
     case '1':
-      modalhtml.something(1);
       console.log(`it reached inside SWITCH - ${buttonClicked}`);
+      modalFunctionFile.insertProjectModalFunction(1);
       break;
     case '2':
-      modalhtml.something(2);
       console.log(`it reached inside SWITCH - ${buttonClicked}`);
+      modalFunctionFile.insertProjectModalFunction(2);
       break;
-    // case '4':
-    //   return (modalContent.innerHTML = modalhtml);
-    // // break;
-    // case '5':
-    //   return (modalContent.innerHTML = modalhtml);
-    // // break;
-    // case '6':
-    //   return (modalContent.innerHTML = modalhtml);
-    // // break;
-    // case '7':
-    //   return (modalContent.innerHTML = modalhtml);
-    // // break;
   }
 
-  modalContent.style.display = 'block';
+  // modalContent.style.display = 'block';
   // modalStructure.style.display = 'block';
 };
 
+const modalClose = event => {
+  document.querySelector('#simpleModal').style.display = 'none';
+};
+
 // 2. Event Listener
-portfolioGrid.addEventListener('click', modalEventHandler);
+portfolioGrid.addEventListener('click', modalOpen);
+document.querySelector('#simpleModal').addEventListener('click', modalClose);
