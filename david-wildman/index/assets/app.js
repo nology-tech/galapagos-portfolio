c3a089d27a54359233693bb72caff24df51abc5c
severine98 <63291308+severine98@users.noreply.github.com>
2020-07-13 11:30:43 +0000

    Deploying to gh-pages from  @ 7614382b53b62cfe9b4e4422adee58c7c1793aa7 🚀

--- david-wildman/index/assets/app.js
+++ david-wildman/index/assets/app.js
@@ -4,14 +4,19 @@ const bioButtons = document.querySelectorAll('.bio.button');
 const portfolioButtons = document.querySelectorAll('.portfolio.button');
 const contactButtons = document.querySelectorAll('.contact.button');
 const homePage = document.querySelector('.home.page');
-const headerTitle = document.querySelector('.header.title');
 const homePageButtons = document.querySelectorAll('.home-nav li');
-const bioPage = document.querySelector('.bio.page');
-const portfolioPage = document.querySelector('.portfolio.page');
+const headerTitle = document.querySelector('.header.title');
+let pageArticles;
+let pageTitle;
+// const bioPage = document.querySelector('.bio.page');
+// const bioPageTitle = document.querySelector('.bio.title');
+// const portfolioPage = document.querySelector('.portfolio.page');
+// const portfolioPageTitle = document.querySelector('.porfolio.title');
 const portfolioLinks = document.querySelectorAll('h6');
 const contactPage = document.querySelector('.contact.page');
 const footer = document.querySelector('.footer');
 
+const expandButtons = document.querySelectorAll('.article-footer-text');
 const addEventListeners = (buttons, clickBehaviour) => {
   buttons.forEach(button => {
     button.addEventListener('click', () => clickBehaviour(event));
@@ -26,13 +31,19 @@ const playBeep = (filePath) => {
 
 
 const updatePage = (event) => {
-  playBeep('./index/assets/audio/lowbeep.mp3');
-  let livePage = document.querySelector('.on');
-  let newPage = document.querySelector(`.${event.currentTarget.classList[0]}.page`);
-  setTimeout(() => {
-    livePage.classList.remove('on');
-    newPage.classList.add('on');
-  }, 2000);
+  if (document.querySelector('.on') === null) {
+    loadMainMenu();
+  } else {
+    playBeep('./index/assets/audio/lowbeep.mp3');
+    let livePage = document.querySelector('.on');
+    let newPage = document.querySelector(`.${event.currentTarget.classList[0]}.page`);
+    setTimeout(() => {
+      livePage.classList.remove('on');
+      newPage.classList.add('on');
+      console.log(newPage.classList[0]);
+      pageFadeIn(newPage.classList[0]);
+    }, 2000);
+  }
 };
 
 
@@ -43,8 +54,8 @@ const mainMenuFadeIn = (event) => {
     homePageButton.style.transitionDelay = `${index * 0.4}s`;
     homePageButton.style.opacity = '1';
     homePageButton.style.transform = 'scale(1)';
-    headerTitle.style.opacity = '1';
   })), 500);
+  setTimeout(() => headerTitle.style.opacity = '1', 500);
 }
 
 const mainMenuFadeOut = (event) => {
@@ -65,6 +76,30 @@ const mainMenuFadeOut = (event) => {
 };
 
 
+const pageFadeIn = (newPage) => {
+  pageArticles = document.querySelectorAll(`.${newPage} article`);
+  pageTitle = document.querySelector(`.${newPage}.title`);
+  console.log(pageArticles);
+  setTimeout((() => pageArticles.forEach((article, index) => {
+    article.style.transitionDelay = `${index * 0.4}s`;
+    article.style.opacity = '1';
+    article.style.transform = 'scale(1)';
+  })), 500);
+  setTimeout(() => pageTitle.style.opacity = '1', 500);
+}
+
+const pageFadeOut = (page, event) => {
+  event.currentTarget.style.color = '#8ea8c3';
+  event.currentTarget.style.borderBottom = '2px solid #ee6352';
+  `.${page}pageButtons`.forEach((button, index) => {
+    if (button != event.currentTarget) {
+      button.style.transitionDelay = '1.5s';
+      button.style.opacity = '0';
+      button.style.transform = 'scale(1.5)';
+    }
+    `.${page}PageTitle`.style.opacity = '0';
+  });
+};
 
 /* Navigation functions */
 
@@ -78,6 +113,19 @@ const loadMainMenu = (event) => {
     headerTitle.style.opacity = '1';
   }, 2000);
 };
+
+const expandArticle = (event) => {
+  event.currentTarget.parentNode.parentNode.childNodes[3].classList.toggle('open');
+  event.currentTarget.parentNode.parentNode.childNodes[5].classList.toggle('open');
+  if (event.currentTarget.innerText == "＋") {
+    event.currentTarget.innerText = "－";
+    playBeep('./index/assets/audio/mediumbeep.mp3');
+  } else {
+    event.currentTarget.innerText = "＋";
+    playBeep('./index/assets/audio/highbeep.mp3');
+
+  };
+}
 
 addEventListeners(homePageButtons, mainMenuFadeOut);
 addEventListeners(homeButtons, loadMainMenu);
@@ -93,10 +141,7 @@ addEventListeners(bioButtons, updatePage);
 addEventListeners(portfolioButtons, updatePage);
 addEventListeners(contactButtons, updatePage);
 
-
-loadMainMenu();
-
-
+addEventListeners(expandButtons, expandArticle);
 
 // const landingPage = document.querySelector('.landing.page');
 
@@ -111,4 +156,6 @@ loadMainMenu();
 ////    footer.style.height = '8vh';
 // footer.style.opacity = '1';
 
-console.log(bioPage, portfolioPage, portfolioLinks, contactPage, footer, event);
+// console.log(bioPage, portfolioPage, portfolioLinks, contactPage, footer, event);
+
+updatePage();
